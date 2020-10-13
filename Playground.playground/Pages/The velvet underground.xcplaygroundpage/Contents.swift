99ed21507f7959039ee12a53879cdc86cc92a7df
Rsun <rsun@lcs.on.ca>
2020-10-13 12:13:54 -0400

    2020.10.13  Starting the 4th poster "the velvet underground". Figure out the loop and the expression

--- Playground.playground/Pages/The velvet underground.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/The velvet underground.xcplaygroundpage/Contents.swift
@@ -1,7 +1,91 @@
-//: [Previous](@previous)
+//: [Previous](@previous) / [Next](@next)
+//: # Blank canvas to copy
+//:
+//: ## To duplicate this page
+//:
+//: Place your cursor on line 19 of this page, press `Command-A` to select all text, and then `Command-C` to copy.
+//:
+//: Two-finger tap on the playground, then choose **New Playground Page**:
+//:
+//: ![new-playground](new-playground.png "New Playground")
+//:
+//: Move to the new page and press `Command-A` to select all text there, then `Command-V` to paste.
+//:
+/*:
+ ## Canvas size
+ 
+ Set the size of your desired canvas by adjusting the constants on lines 19 and 20.
+ */
+let preferredWidth = 300
+let preferredHeight = 600
+/*:
+ ## Required code
+ 
+ Lines 28 to 36 are required to make the playground run.
+ 
+ Please do not remove.
+ */
+import Cocoa
+import PlaygroundSupport
+import CanvasGraphics
 
-import Foundation
+// Create canvas
+let canvas = Canvas(width: 400 , height: 600)
 
-var str = "Hello, playground"
+// Show the canvas in the playground's live view
+PlaygroundPage.current.liveView = canvas
 
-//: [Next](@next)
+
+/*:
+ ## Add your code
+ 
+ Beginning on line 48, write a meaningful comment.
+ 
+ You can remove the code on line 49 and begin writing your own code.
+ 
+ [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
+
+ */
+let black = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
+let purple = Color(hue: 309, saturation: 78, brightness: 64, alpha: 100)
+let offWhite = Color(hue: 83, saturation: 4, brightness: 89, alpha: 100)
+
+
+canvas.defaultLineWidth = 15
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+
+for x in stride(from: 10, to: 410, by: 80){
+    for y in stride(from: 10, to: 410, by: 80){
+        
+        if x == 0 {
+            canvas.lineColor = offWhite
+        } else if x-y == 0{
+            canvas.lineColor = offWhite
+        } else {
+            canvas.lineColor = purple
+        }
+        
+        canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x+45, y: y))
+        canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x, y: y+45))
+        canvas.drawLine(from: Point(x: x+5, y: y+5), to: Point(x: x+60 , y: y+60))
+        
+        
+    }
+}
+
+/*:
+ ## Show the Assistant Editor
+ Don't see any results?
+ 
+ Remember to show the Assistant Editor (1), and then switch to Live View (2):
+ 
+ ![timeline](timeline.png "Timeline")
+
+ ## Use source control
+ To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
+ 
+ Please commit and push your work often.
+ 
+ ![source_control](source-control.png "Source Control")
+ */
