ec8d280b8ba899737f2591b31751fdbc5895a2d6
Rsun <rsun@lcs.on.ca>
2020-10-16 09:06:37 -0400

    2020.10.16 Finish the first Brain Builder Project. I learned that you need to use different variable when you try to make the shape move to different direction.

--- Animation/FiveCircles.swift
+++ Animation/FiveCircles.swift
@@ -18,6 +18,7 @@ class FiveCircles: NSObject, Sketchable {
     
     //Vertical position
     var x = 250
+    var c = 250
     
     // This function runs once
     override init() {
@@ -30,13 +31,27 @@ class FiveCircles: NSObject, Sketchable {
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
        
-       //Vertical position
-        x += 1
-        
-       //First Cicrcle
         canvas.drawShapesWithBorders = false
+        
+        //Second, Fourth Circle
+        c -= 1
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(at: Point(x: c, y: 350), width: 50, height: 50)
+        c -= 1
+        canvas.fillColor = Color.blue
+        canvas.drawEllipse(at: Point(x: c, y: 150), width: 50, height: 50)
+
+        //First, Third, Fifth Cicrcle
+        x += 1
         canvas.fillColor = Color.purple
         canvas.drawEllipse(at: Point(x: x, y: 450), width: 50, height: 50)
+        x += 1
+        canvas.fillColor = Color.green
+        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
+        x += 1
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(at: Point(x: x, y: 50), width: 50, height: 50)
+
     }
     
 }
