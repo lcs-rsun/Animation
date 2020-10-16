4e5da6ce7264420f7e88df1aa46321a67fa109d5
Rsun <rsun@lcs.on.ca>
2020-10-16 10:03:28 -0400

    2020.10.16 Staring the Brain Builder#3

---
+++ Animation/BouncingCircle.swift
@@ -0,0 +1,38 @@
+//
+//  EmptySketch.swift
+//  Animation
+//
+//  Created by Russell Gordon on 2020-06-08.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+import CanvasGraphics
+
+// NOTE: This is a completely empty sketch; it can be used as a template.
+class BouncingCircle: NSObject, Sketchable {
+    
+    // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
+    //       Therefore, the line immediately below must always be present.
+    var canvas: Canvas
+    
+    var x = 250
+    // This function runs once
+    override init() {
+        
+        // Create canvas object – specify size
+        canvas = Canvas(width: 500, height: 500)
+                
+    }
+    
+    // This function runs repeatedly, forever, to create the animated effect
+    func draw() {
+        
+        x += 1
+        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
+        
+        
+    }
+    
+}
+
