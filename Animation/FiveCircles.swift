7561530e7ac94e2465d184961620ab2c930d8f5a
Rsun <rsun@lcs.on.ca>
2020-10-16 08:46:21 -0400

    2020.10.16 Make the first circle move to the right.

--- Animation/FiveCircles.swift
+++ Animation/FiveCircles.swift
@@ -1,5 +1,5 @@
 //
-//  BasicSketch.swift
+//  EmptySketch.swift
 //  Animation
 //
 //  Created by Russell Gordon on 2020-06-08.
@@ -9,38 +9,35 @@
 import Foundation
 import CanvasGraphics
 
-class BasicSketch: NSObject, Sketchable {
+// NOTE: This is a completely empty sketch; it can be used as a template.
+class FiveCircles: NSObject, Sketchable {
     
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     var canvas: Canvas
     
-    // Position of circle
-    var x: Int
+    //Vertical position
+    var x = 250
     
     // This function runs once
     override init() {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
-        
-        // Set starting position
-        x = 250
-        
-        // Slow down the animation
-        canvas.framesPerSecond = 10
-        
+                
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
+       
+       //Vertical position
+        x += 1
         
-        // Change position
-        x -= 1
-        
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
-        
+       //First Cicrcle
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.purple
+        canvas.drawEllipse(at: Point(x: x, y: 450), width: 50, height: 50)
     }
     
 }
+
--- Animation/MovingSquare.swift
+++ Animation/MovingSquare.swift
@@ -1,5 +1,5 @@
 //
-//  BasicSketch.swift
+//  EmptySketch.swift
 //  Animation
 //
 //  Created by Russell Gordon on 2020-06-08.
@@ -9,38 +9,33 @@
 import Foundation
 import CanvasGraphics
 
-class BasicSketch: NSObject, Sketchable {
+// NOTE: This is a completely empty sketch; it can be used as a template.
+class MovingSquare: NSObject, Sketchable {
     
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     var canvas: Canvas
     
-    // Position of circle
-    var x: Int
+    //Vertical position
+    var y = 250
     
     // This function runs once
     override init() {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
-        
-        // Set starting position
-        x = 250
-        
-        // Slow down the animation
-        canvas.framesPerSecond = 10
-        
+                
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x -= 1
+        //Change the vertical position
+        y += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
-        
+        //Draw a square in the middle of the canvas
+        canvas.drawRectangle(at: Point(x: 250, y: y), width: 50, height: 50, anchoredBy: .centre)
     }
     
 }
+
