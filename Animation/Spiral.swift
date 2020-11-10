e4a2dd9f70e95523e353f3a58e015d886291f1e5
Rsun <rsun@lcs.on.ca>
2020-11-10 12:09:24 -0500

    2020.11.10. Got one spiral working!

--- Animation/Spiral.swift
+++ Animation/Spiral.swift
@@ -1,4 +1,3 @@
-//
 //  EmptySketch.swift
 //  Animation
 //
@@ -12,23 +11,47 @@ import CanvasGraphics
 // NOTE: This is a completely empty sketch; it can be used as a template.
 class Spiral: NSObject, Sketchable {
     
+
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     var canvas: Canvas
     
+    // Add one spiral
+    var spiral: IndividualSpiral
+
     // This function runs once
     override init() {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
                 
+        // Give the one spiral a starting angle of rotation
+        spiral = IndividualSpiral(angleOffset: 0)
+        
+        // Speed
+        canvas.framesPerSecond = 80
     }
     
+
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
+
+        // What frame are we on?
+//        print(canvas.frameCount)
         
+        canvas.defaultLineWidth = 1
+        
+        // Set the origin to be the middle of the canvas
+        canvas.translate(to: Point(x: canvas.width / 2, y: canvas.height / 2))
+        
+        // Update the position of that one spiral
+        spiral.update(on: canvas)
+        
+
+
+
     }
     
-}
 
+}
